fb2af4be3e718e84c6c4ab9d4b7e447b734dd3d8
ceitel <ceitel@live.com>
2016-10-04 11:27:50 -0600

    added data structure definition for passing URL and SS list
    "What to turn in #2
    awget.h: this file should contain the definition of the data structure for passing the URL and the SS list between awget and SS, and between SS and SS. The data structure should contain the URL, the number of SS entries and the SSaddr and SSport for each entry."

--- awget.h
+++ awget.h
@@ -3,6 +3,15 @@
 //Dr. Indrajit Ray
 //10/27/2016
 
-int main(int argc, char* argv[]){
+#ifndef _AWGET_H_INCLUDED_   // if hasn't been included yet...
+#define _AWGET_H_INCLUDED_   // #define this so the compiler knows it has been included
 
-}
+//packet structure for url+chain packet
+//example: url_len=14,chain_sz=3 data="www.google.com123.21.1,3600,123.45.2,3601,123.67.3,3602"
+typedef struct{
+	short url_len; //2bytes
+	short chain_sz; //2bytes
+	char* data; //nbytes
+} chain_packet;
+
+#endif
