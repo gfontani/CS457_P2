597e6d1ef1de78d0a17e0ed814bbed13935df0f9
ceitel <ceitel@live.com>
2016-10-18 19:24:28 -0600

    Update awget.h

--- awget.h
+++ awget.h
@@ -6,18 +6,15 @@
 #ifndef _AWGET_H_INCLUDED_   // if hasn't been included yet...
 #define _AWGET_H_INCLUDED_   // #define this so the compiler knows it has been included
 
-#define MAX_CHUNK_SIZE 65535	//this value should be <= the maximum size allowed through a socket connection in one "send"
+#define MAX_CHUNK_SIZE 65000	//this value should be <= the maximum size allowed through a socket connection in one "send"
 
 //packet structure for url+chain packet
-//example: url_len=14,chain_sz=3 data="www.google.com123.21.1,3600,123.45.2,3601,123.67.3,3602"
+//example: url_len=14,chain_sz=3 data="www.google.com" data2="123.21.1,3600,123.45.2,3601,123.67.3,3602"
 typedef struct{
-	long data_sz; //size of incoming file in bytes (used to allocate memory for incoming file)
-	//short total_chunks; //would replace data_sz. Easier to implement?
-	short chunk_no; //use this number to memcopy to the correct place in memory for large file (chunk_no * MAX_CHUNK_SIZE)
-	//short chunk_sz; //chunk_sz <= to MAX_CHUNCK_SIZE, not necessary if we just allocate MAX_CHUNCK_SIZE for each packet
-	short url_len; //url_len == 0 would indicate that msg is a file and not a chain packet)
-	short chain_sz; //if url_len != 0, then chain_sz == 0 indicates last SS in chain)
-	char* data; //nbytes
+	short size1; //URL length or chunk_no
+	short size2; //either chain length or total_chunks
+	char* data1; //URL or chunk
+	char* data2; //either chainlist or null
 } packet;
 
 #endif
